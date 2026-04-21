eaca3a8d7c9a42507fa8fe76326cbe131450d7f7
Adam Cwikla <adamcwikla04@gmail.com>
2026-04-21 18:45:00 +0200

    dodanie swaggera

--- API-pokoje-s33979/Program.cs
+++ API-pokoje-s33979/Program.cs
@@ -1,19 +1,28 @@
 using API_pokoje_s33979;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 
+// builder.Services.AddEndpointsApiExplorer(); // Wymagane, by Swagger "widział" endpointy
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(s =>
+    {
+        s.SwaggerEndpoint("openapi/v1.json", "v1");
+        s.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseAuthorization();
